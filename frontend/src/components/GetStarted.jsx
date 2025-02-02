3838a8083a10324bcd051a0342dd6d470ccec747
Subhrajit Saha <subhoxsaha@gmail.com>
2025-02-02 13:27:14 +0530

    updated

--- frontend/src/components/GetStarted.jsx
+++ frontend/src/components/GetStarted.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom'
 const GetStarted = () => {
   return (
     <div className='w-screen h-full flex justify-center'>
-      <div><h1 className='text-2xl mt-10'>Use this To-do application to track your desired todos <Link className='text-blue-600 underline underline-offset-4'> Login in now</Link>  to use</h1>
+      <div><h1 className='text-2xl mt-10'>Use this To-do application to track your desired todos <Link to='/login' className='text-blue-600 underline underline-offset-4'> Login in now</Link>  to use</h1>
       </div>
 
       
